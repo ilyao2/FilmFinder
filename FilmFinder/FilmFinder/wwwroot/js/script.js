1f6da5112969a46820538b57f7b16b2c832f641d
Илья Обыденный <ilya.obydennye@gmail.com>
2020-08-03 02:30:53 +0700

    JS refactoring

--- FilmFinder/FilmFinder/wwwroot/js/script.js
+++ FilmFinder/FilmFinder/wwwroot/js/script.js
@@ -8,31 +8,26 @@ for (i = 0; i < elements.length; i++){
 
 Height = elements[0].offsetHeight;
 Width = elements[0].offsetWidth;
-elements[0].onclick = function(e) {
-	var y = e.offsetY==undefined?e.layerY:e.offsetY;
-  	var x = e.offsetX==undefined?e.layerX:e.offsetX;
-  	alert(e.offsetX)
-}
+
 function overEvent(e) {
 	element = e.currentTarget;
 	over(element);
-
 }
 
 function over(element){
 	var start = Date.now(); // запомнить время начала
 
 	var timer = setInterval(function() {
-	  // сколько времени прошло с начала анимации?
-	  var timePassed = Date.now() - start;
+	// сколько времени прошло с начала анимации?
+	var timePassed = Date.now() - start;
 
-	  if (timePassed >= 200) {
-	    clearInterval(timer);
-	  }
+	if (timePassed >= 200) {
+		clearInterval(timer);
+	}
 
-	  progress = timePassed/200
-		element.style.width = (Width * (1 + progress * 0.25) + 'px');
-		element.style.height = (Height * (1 + progress * 0.25) + 'px');
+	progress = timePassed/200
+	element.style.width = (Width * (1 + progress * 0.25) + 'px');
+	element.style.height = (Height * (1 + progress * 0.25) + 'px');
 
 	}, 10);
 
@@ -50,24 +45,27 @@ function out(element){
 	element.children[0].style.boxShadow = 10 + 'px ' + 10 + 'px 10px #000';
 	var start = Date.now(); // запомнить время начала
 	var timer = setInterval(function() {
-	  // сколько времени прошло с начала анимации?
-	  var timePassed = Date.now() - start;
+	// сколько времени прошло с начала анимации?
+	var timePassed = Date.now() - start;
 
-	  if (timePassed > 200) {
-	    clearInterval(timer);
-	  }
+	if (timePassed > 200) {
+		clearInterval(timer);
+		element.style.width = Width;
+		element.style.height = Height;
+		return;
+	}
 
-		progress = timePassed / 200;
-		element.style.width = (Width * (1 + (1 - progress) * 0.25) + 'px');
-		element.style.height = (Height * (1 + (1 - progress) * 0.25) + 'px');
+	progress = timePassed / 200;
+	element.style.width = (Width * (1 + (1 - progress) * 0.25) + 'px');
+	element.style.height = (Height * (1 + (1 - progress) * 0.25) + 'px');
 
 	}, 10);
 }
 
 function moveEvent(e) {
-  element = e.currentTarget.children[0];
-  var x = event.pageX - e.currentTarget.offsetLeft;
-  var y = event.pageY - e.currentTarget.offsetTop;
+	element = e.currentTarget.children[0];
+	var x = event.pageX - e.currentTarget.offsetLeft;
+	var y = event.pageY - e.currentTarget.offsetTop;
 	nY = y / e.currentTarget.offsetHeight/1.5;
 	nX = x / e.currentTarget.offsetWidth/1.5;
 
@@ -76,26 +74,26 @@ function moveEvent(e) {
 	degX = 0.0;
 	degY = 0.0;
 	deg = 70;
-  if(nY > 0.5){
-	  degX = (nY - 0.5) * deg;
-  }
-  else{
-	  degX = -(0.5 - nY) * deg;
-  }
+	if(nY > 0.5){
+		degX = (nY - 0.5) * deg;
+	}
+	else{
+		degX = -(0.5 - nY) * deg;
+	}
 
-  if(nX > 0.5){
-	  degY = -(nX - 0.5) * deg;
-  }
-  else{
-	  degY = (0.5 - nX) * deg;
-  }
+	if(nX > 0.5){
+		degY = -(nX - 0.5) * deg;
+	}
+	else{
+		degY = (0.5 - nX) * deg;
+	}
 
 	shadowY = (1 - nY) * 30;
 	shadowX = (1 - nX) * 30;
 
-  element.style.filter = 'brightness(' + brightness + ')';
+	element.style.filter = 'brightness(' + brightness + ')';
   //rotate3d(x, y, z, angle)
-  element.style.transform = 'rotateX(' + degX + 'deg)' + ' rotateY(' + degY + 'deg)';
+	element.style.transform = 'rotateX(' + degX + 'deg)' + ' rotateY(' + degY + 'deg)';
   //element.style.transform = 'rotate3d(0,1,0,' + degY +'deg) rotate3d(1,0,0,' + degX +'deg)'
 	element.style.boxShadow = shadowX + 'px ' + shadowY + 'px 10px #000';
 }
