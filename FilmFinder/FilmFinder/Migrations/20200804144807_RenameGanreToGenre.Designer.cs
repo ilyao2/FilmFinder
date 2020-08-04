b9655c008c1d8ac51e0c3d5341783a5e3f26f780
Илья Обыденный <ilya.obydennye@gmail.com>
2020-08-04 22:08:08 +0700

    Added initObjects, BugFix with DB after rename genre

--- FilmFinder/FilmFinder/Migrations/20200804144807_RenameGanreToGenre.Designer.cs
+++ FilmFinder/FilmFinder/Migrations/20200804144807_RenameGanreToGenre.Designer.cs
@@ -4,14 +4,16 @@ using FilmFinder.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FilmFinder.Migrations
 {
     [DbContext(typeof(DbContent))]
-    partial class DbContentModelSnapshot : ModelSnapshot
+    [Migration("20200804144807_RenameGanreToGenre")]
+    partial class RenameGanreToGenre
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- FilmFinder/FilmFinder/Startup.cs
+++ FilmFinder/FilmFinder/Startup.cs
@@ -49,25 +49,106 @@ namespace FilmFinder
             app.UseStaticFiles();
             app.UseMvcWithDefaultRoute();
 
-            /*DbContent db;
+            DbContent db;
             using (var scope = app.ApplicationServices.CreateScope())
             {
                 db = scope.ServiceProvider.GetRequiredService<DbContent>();
-                Genre g1 = new Genre { name = "Fantasy" };
-                Film f1 = new Film { name = "Pirates of the Caribbean", ganre =  g1 };
-                Actor a1 = new Actor { fullName = "John Christopher «Johnny» Depp" };
+                initObjects(db);
+            }
+            
+            
+        }
 
-                db.Add(g1);
+        void initObjects(DbContent db)
+        {
+            List<Genre> genres = db.Genre.ToList();
+            List<Actor> actors = db.Actor.ToList();
+            List<Film> films = db.Film.ToList();
+            if (!db.Genre.Any())
+            {
+                genres = new List<Genre>
+                {
+                    new Genre { name = "Fantasy"},
+                    new Genre { name = "Drama"},
+                    new Genre { name = "Triller"},
+                    new Genre { name = "Western"},
+                };
+                db.AddRange(genres);
                 db.SaveChanges();
-                db.Add(f1);
+            }
+            if (!db.Actor.Any())
+            {
+                actors = new List<Actor>
+                {
+                    new Actor { fullName = "John Cristopher \"Johnny\" Depp" },
+                    new Actor { fullName = "Armand Douglas \"Armie\" Hammer" },
+                    new Actor { fullName = "Keanu Charles Reeves" },
+                    new Actor { fullName = "Michael Nyqvist" },
+                    new Actor { fullName = "Laurence J. Fishburne" },
+                    new Actor { fullName = "Tom Hanks" },
+                    new Actor { fullName = "Robin Virginia Gayle Wright" },
+                    new Actor { fullName = "Edward Harrison Norton" },
+                    new Actor { fullName = "William Bradley \"Brad\" Pitt" },
+                    new Actor { fullName = "David Morse" },
+                    new Actor { fullName = "Tim Robbins" },
+                    new Actor { fullName = "Morgan Freeman" },
+
+                };
+                db.AddRange(actors);
                 db.SaveChanges();
-                db.Add(a1);
+            }
+            if (!db.Film.Any())
+            {
+
+                films.Add(new Film { name = "Pirates of the Caribbean on stranger tides", genre = genres.First(g => g.name.Contains("Fantasy")), imgPath = "PotC.jpg" });
+                films.Add(new Film { name = "The Shawshank Redemption", genre = genres.First(g => g.name.Contains("Drama")), imgPath = "TSR.jpg" });
+                films.Add(new Film { name = "The Green Mile", genre = genres.First(g => g.name.Contains("Drama")), imgPath = "TGM.jpg" });
+                films.Add(new Film { name = "Fight Club", genre = genres.First(g => g.name.Contains("Triller")), imgPath = "FC.jpg" });
+                films.Add(new Film { name = "Forrest Gump", genre = genres.First(g => g.name.Contains("Drama")), imgPath = "FG.jpg" });
+                films.Add(new Film { name = "The Matrix", genre = genres.First(g => g.name.Contains("Fantasy")), imgPath = "TM.jpg" });
+                films.Add(new Film { name = "John Wick", genre = genres.First(g => g.name.Contains("Triller")), imgPath = "JW.jpg" });
+                films.Add(new Film { name = "The Lone Ranger", genre = genres.First(g => g.name.Contains("Western")), imgPath = "TLR.jpg" });
+        
+                db.AddRange(films);
                 db.SaveChanges();
-                f1.FilmActor.Add(new FilmActor { FilmId = f1.id, ActorId = a1.id }); ;
+                Film film = null;
+                film = films.First(f => f.name.Contains("Pirates"));
+                film.FilmActor.Add(new FilmActor { FilmId = film.id, ActorId = actors.First(a => a.fullName.Contains("Depp")).id});
+
+
+                film = films.First(f => f.name.Contains("Shawshank"));
+                film.FilmActor.Add(new FilmActor { FilmId = film.id, ActorId = actors.First(a => a.fullName.Contains("Tim")).id });
+                film.FilmActor.Add(new FilmActor { FilmId = film.id, ActorId = actors.First(a => a.fullName.Contains("Morgan")).id });
+
+
+                film = films.First(f => f.name.Contains("Green"));
+                film.FilmActor.Add(new FilmActor { FilmId = film.id, ActorId = actors.First(a => a.fullName.Contains("Tom")).id });
+                film.FilmActor.Add(new FilmActor { FilmId = film.id, ActorId = actors.First(a => a.fullName.Contains("David")).id });
+
+
+                film = films.First(f => f.name.Contains("Fight"));
+                film.FilmActor.Add(new FilmActor { FilmId = film.id, ActorId = actors.First(a => a.fullName.Contains("Edward")).id });
+                film.FilmActor.Add(new FilmActor { FilmId = film.id, ActorId = actors.First(a => a.fullName.Contains("Brad")).id });
+
+                film = films.First(f => f.name.Contains("Forrest"));
+                film.FilmActor.Add(new FilmActor { FilmId = film.id, ActorId = actors.First(a => a.fullName.Contains("Tom")).id });
+                film.FilmActor.Add(new FilmActor { FilmId = film.id, ActorId = actors.First(a => a.fullName.Contains("Robin")).id });
+
+                film = films.First(f => f.name.Contains("Matrix"));
+                film.FilmActor.Add(new FilmActor { FilmId = film.id, ActorId = actors.First(a => a.fullName.Contains("Keanu")).id });
+                film.FilmActor.Add(new FilmActor { FilmId = film.id, ActorId = actors.First(a => a.fullName.Contains("Laurence")).id });
+
+                film = films.First(f => f.name.Contains("Wick"));
+                film.FilmActor.Add(new FilmActor { FilmId = film.id, ActorId = actors.First(a => a.fullName.Contains("Keanu")).id });
+                film.FilmActor.Add(new FilmActor { FilmId = film.id, ActorId = actors.First(a => a.fullName.Contains("Michael")).id });
+
+                film = films.First(f => f.name.Contains("Lone"));
+                film.FilmActor.Add(new FilmActor { FilmId = film.id, ActorId = actors.First(a => a.fullName.Contains("Depp")).id });
+                film.FilmActor.Add(new FilmActor { FilmId = film.id, ActorId = actors.First(a => a.fullName.Contains("Armand")).id });
+
                 db.SaveChanges();
-            }*/
-            
-            
+            }
+
         }
     }
 }
